56dcd87ac9990bf23552b64b14940e0f54b98849
Sunning <kelvinpaez2004@gmail.com>
2023-06-03 15:20:29 -0400

    Probar

--- task1/routes/index.js
+++ task1/routes/index.js
@@ -6,12 +6,15 @@ const { request } = require('http');
 const { json } = require('body-parser');
 const geoip = require('geoip-lite')
 const nodemailer = require('nodemailer')
-require('dotenv').config()
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+const usuario = process.env.EMAIL_USER2
+const passha = process.env.EMAIL_PASS2
 
 router.post('/', function(req, res, next) {
   let name = req.body.name;
@@ -24,32 +27,34 @@ router.post('/', function(req, res, next) {
   // let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // @todo falta formatear la ip
   let loc
    // replace with the IP address you want to look up
-  const geo = geoip.lookup("186.92.82.190");
+  const geo = geoip.lookup(ip);
   loc = (geo.country);
 
   db.insert(name, email, comment, now, ip, loc);
-
+  
   //Crear Transportador de correo
   var transporter = nodemailer.createTransport ({
+    
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     // service: 'gmail',
     auth: {
       // type: 'PLAIN',
-      user: process.env.USER,
-      pass: process.env.PASS
-      // user: 'kendalltrece@gmail.com',
-      // pass: 'wolootvvtmvrwjri'
+      // user: usuario,
+      // pass: passha
+      user: 'kendalltrece@gmail.com',
+      pass: 'wolootvvtmvrwjri'
     }
   });
 
   // Crear un objeto de opciones de correo
   var mailOptions = {
     nombre: name,
+    from: email,
     to: "kelvinpaez2004@gmail.com",
     subject: 'Contacto desde el formulario',
-    text: comment
+    text: "Enviado por " + name + "\nEmail: " + email + "\nMensaje: " + comment
   };
 
   // Enviar el correo electrónico con el transportador
@@ -71,7 +76,8 @@ router.get('/contactos', function(req, res, next) {
   db.select(function (rows) {
     console.log(rows);
   });
-  res.send('ok');
+  res.send('ok' + process.env);
+  console.log(dotenv.config().parsed)
 });
 
 
